4cc0dafd6cb474d2f105041b23b96e26c028aadb
Monish Gowda <monishgowdamg7@gmail.com>
2026-03-26 19:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (90.50%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        r=s.strip()
+        v=r.split(" ")
+        
+        p=len(v)
+        m=[]
+        for i in range(p):
+            # for j in v:
+            if i==p-1:
+               
+                for j in v[p-1]:
+                    m.append(j)
+ 
+                return len(m)    
+
+
+
+
+
+                # for j in v:
+                #     for char in j:
+                #         m.append(char)
+
+                #     return len(m)            
+ 
+
+
+            # if i==len(v):
+            #     for char in i:
+            #         m.append(i)
+
+            # return len(m)
+
+
