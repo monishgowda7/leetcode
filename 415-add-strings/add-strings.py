97955ee8d6d329f683aface430ee7aadd647dd45
Monish Gowda <monishgowdamg7@gmail.com>
2026-03-28 15:31:18 +0530

    Time: 3 ms (66.19%) | Memory: 12.3 MB (91.33%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        m1=int(num1)
+        m2=int(num2)
+        m=m1+m2
+        return str(m)
