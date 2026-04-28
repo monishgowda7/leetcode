4f4d4726290ecf3c32d2206d9ab021b367d14ff0
Monish Gowda <monishgowdamg7@gmail.com>
2026-04-28 17:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.36%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,18 +5,26 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        for i in nums:
-            if i==val:
-                nums.remove(i)
-                # nums.append(_)    
+        # for i in nums:
+        #     if i==val:
+        #         nums.remove(i)
+        #         # nums.append(_)    
 
-        for i in nums:
-            if i==val:
-                nums.remove(i)
+        # for i in nums:
+        #     if i==val:
+        #         nums.remove(i)
         
         
-        for i in nums:
-            if i==val:
-                nums.remove(i)
+        # for i in nums:
+        #     if i==val:
+        #         nums.remove(i)
 
-        return len(nums)        
+        # return len(nums)        
+        k=0
+        for i in range(0,len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+                
+
+        return k       
